d7cdba87270eb90ef26b734bcba39e2f3d06002e
CedLFMarDev <cedlfmardev@users.noreply.github.com>
2026-07-11 18:53:24 -0300

    Remove duplicate kernel source files from nested folder

--- KernelSO-main/fb.h
+++
@@ -1,8 +0,0 @@
-#ifndef INCLUDE_FB_H
-#define INCLUDE_FB_H
-
-void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg);
-void fb_move_cursor(unsigned short pos);
-void fb_write(const char *buf, unsigned int len, unsigned char fg, unsigned char bg);
-
-#endif
